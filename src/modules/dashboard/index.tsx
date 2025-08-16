7b3ffbb32d46d78d7ae6d43ab90bfc452d7c9993
Joecrypt <joecrypt@yahoo.com>
2025-08-16 23:44:52 +0100

    feat: completed overview cards

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,15 +1,20 @@
 import DashboardLayout from "@/components/layout/dashboard-layout";
 import SalesOverview from "./components/sales-overview";
+import { user } from "@/hooks/auth";
+import OverviewCards from "./components/overview-cards";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
       <h3 className="text-xl/[100%] font-semibold text-dark font-dm-sans">
-        Welcome, Joseph
+        Welcome, {user?.name ?? "User"}
       </h3>
       <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="col-span-1 lg:col-span-2">
           <SalesOverview />
+        </div>
+        <div>
+          <OverviewCards />
         </div>
       </div>
     </DashboardLayout>
